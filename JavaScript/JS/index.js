fb7efc5a111b20ac624dc7799b4468f6d3bd3805
Aydar <51855086+st-aydar@users.noreply.github.com>
2020-10-03 20:26:12 +0200

    training

---
+++ JavaScript/JS/index.js
@@ -0,0 +1,52 @@
+// comment
+/* comment
+comment
+Data types: 
+undefined, null, boolean, string, symbol, number, object
+*/
+console.log("Hi");
+console.log("test console");
+var myName = "Name";
+
+myName = 8;
+
+let ourName = "freecodecamp";
+
+const pi = 3.14; // never changes
+
+// assign var
+var a; // declare
+var b = 2; // declare and assign
+a = 8; // assign
+
+b = a;
+a++;
+
+console.log(a);
+
+b--;
+console.log(b);
+
+var remainder = 11 % 3;
+console.log("remainder = ", remainder);
+
+a -= 6; // substruct 6 from a
+console.log(a);
+
+var myStr = 'I am a "double quotation" string';
+console.log(myStr);
+/***** 
+\'
+\"
+\\
+\n new line
+\r carriage return 
+\t tab
+\b backspace
+\f formfeed
+*****/
+
+var ourStr = "I come first. \n" + "I come second.";
+stringLength = ourStr.length;
+firstLetter = ourStr[0];
+console.log(ourStr);
